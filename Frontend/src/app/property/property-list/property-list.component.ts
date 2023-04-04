dcd6761fabe7c92eb4fcba9ec1a1fd029197c06b
Fausto Torres <faut.btorres@gmail.com>
2023-04-05 00:08:32 +0100

    adding styles and getting data using https

--- Frontend/src/app/property/property-list/property-list.component.ts
+++ Frontend/src/app/property/property-list/property-list.component.ts
@@ -1,59 +1,21 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { HousingService } from 'src/app/services/housing.service';
 
 @Component({
   selector: 'app-property-list',
   templateUrl: './property-list.component.html',
   styleUrls: ['./property-list.component.css'],
 })
-export class PropertyListComponent {
-  properties: Array<any> = [
-    {
-      Id: 1,
-      Name: 'Birla House',
-      Type: 'House',
-      Price: 12000,
-    },
-    {
-      Id: 2,
-      Name: 'Erose Villa',
-      Type: 'House',
-      Price: 18400,
-    },
-    {
-      Id: 3,
-      Name: 'Arembepe Hou',
-      Type: 'House',
-      Price: 20000,
-    },
-    {
-      Id: 4,
-      Name: 'Cajacity',
-      Type: 'House',
-      Price: 78050,
-    },
-    {
-      Id: 5,
-      Name: 'Pernambues',
-      Type: 'House',
-      Price: 18700,
-    },
-    {
-      Id: 6,
-      Name: 'Inocoop',
-      Type: 'House',
-      Price: 19580,
-    },
-    {
-      Id: 7,
-      Name: 'Dias Dvila',
-      Type: 'House',
-      Price: 24500,
-    },
-    {
-      Id: 8,
-      Name: 'Candeias',
-      Type: 'House',
-      Price: 10000,
-    },
-  ];
+export class PropertyListComponent implements OnInit {
+  properties: any;
+
+  constructor(private housingService: HousingService) {}
+
+  ngOnInit(): void {
+    this.housingService.getAllProperties().subscribe({
+      next: (data) => (this.properties = data),
+      error: (error) => console.log(error),
+    });
+  }
 }
